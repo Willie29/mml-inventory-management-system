a33ce0c655428073e96b0679733b42881876836f
ArdiTrisnaldi123 <ardi.trisnaldi@syngenta.com>
2023-12-15 22:59:44 +0700

    tidy the all repository modules

--- routers/history.js
+++ routers/history.js
@@ -1,6 +1,5 @@
 const router = require('express').Router()
 const Controller = require('../controllers/history')
-const authorization = require('../middlewares/authorization')
 
 router.get('/', Controller.history)
 
